bf6711cfa74c5b6554cc26d7efca37a1b7c5f867
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-06-09 23:29:24 +0700

    create input widget

---
+++ lib/app/modules/widget/input_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class InputForm extends StatelessWidget {
+  const InputForm(
+      {super.key,
+      required this.labelParam,
+      required this.controllerParam,
+      required this.obsecureParam});
+
+  final String labelParam;
+  final TextEditingController controllerParam;
+  final bool obsecureParam;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: labelParam,
+          hintText: 'Masukan $labelParam disini',
+          hintStyle: GoogleFonts.poppins(),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
