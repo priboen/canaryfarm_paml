9267b838cda9d50608e8025590d5e92329817239
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-06-09 23:54:14 +0700

    make login page and controller for API handling

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,23 +1,56 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:royal_canary_farm_app/api/service/constant.dart';
+import 'package:royal_canary_farm_app/app/modules/home/views/home_view.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
+  final isLoading = false.obs;
+  final token = ''.obs;
+  final box = GetStorage();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  Future login({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      isLoading.value = true;
+      var data = {
+        'username': username,
+        'password': password,
+      };
+
+      var response = await http.post(
+        Uri.parse('${url}login'),
+        headers: {
+          'Accept': 'application/json',
+        },
+        body: data,
+      );
+
+      if (response.statusCode == 200) {
+        isLoading.value = false;
+        token.value = json.decode(response.body)['token'];
+        box.write('token', token.value);
+        Get.offAll(() => const HomeView());
+      } else {
+        isLoading.value = false;
+        Get.snackbar(
+          'Error',
+          json.decode(response.body)['message'],
+          snackPosition: SnackPosition.TOP,
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+        );
+        print(json.decode(response.body));
+      }
+    } catch (e) {
+      isLoading.value = false;
+
+      print(e.toString());
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,22 +1,111 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:royal_canary_farm_app/app/modules/widget/input_widget.dart';
 
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
-  const LoginView({Key? key}) : super(key: key);
+  LoginView({super.key});
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final LoginController loginController = Get.put(LoginController());
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const LoginHeader(),
+            Text("Login Page",
+                style: GoogleFonts.poppins(fontSize: size * 0.080)),
+            Text("Royal Canary Bird Farm",
+                style: GoogleFonts.poppins(fontSize: size * 0.040)),
+            const SizedBox(
+              height: 15,
+            ),
+            InputForm(
+              labelParam: "Username",
+              controllerParam: _usernameController,
+              obsecureParam: false,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            InputForm(
+              labelParam: "Password",
+              controllerParam: _passwordController,
+              obsecureParam: true,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                elevation: 0,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                  vertical: 15,
+                ),
+              ),
+              onPressed: () async {
+                await loginController.login(
+                  username: _usernameController.text.trim(),
+                  password: _passwordController.text.trim(),
+                );
+              },
+              child: Obx(
+                () {
+                  return loginController.isLoading.value
+                      ? const CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                      : Text(
+                          "Login",
+                          style: GoogleFonts.poppins(
+                            fontSize: size * 0.080,
+                          ),
+                        );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+    );
+  }
+}
+
+class LoginHeader extends StatelessWidget {
+  const LoginHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://images-platform.99static.com/pcoX0s_A7IEG7CpjnmLbcLwyH_o=/652x189:1367x904/500x500/top/smart/99designs-contests-attachments/86/86135/attachment_86135086"),
+              radius: 80,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+          ],
         ),
       ),
     );
