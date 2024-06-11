25e199e5beddc7758a01b9425709b07185a8f295
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-06-11 13:56:12 +0700

    make profile page

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,23 +1,63 @@
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:royal_canary_farm_app/api/service/constant.dart';
+import 'package:royal_canary_farm_app/app/data/profile.dart';
+import 'package:royal_canary_farm_app/app/routes/app_pages.dart';
 
 class ProfileController extends GetxController {
-  //TODO: Implement ProfileController
+  var isLoading = false.obs;
+  var profile = Profie(
+    id: null,
+    name: '',
+    address: '',
+    phone: '',
+    photo: '',
+  ).obs;
 
-  final count = 0.obs;
+  final FlutterSecureStorage storage = FlutterSecureStorage();
+
   @override
   void onInit() {
     super.onInit();
+    fetchProfile();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void fetchProfile() async {
+    try {
+      isLoading(true);
+      final token = await storage.read(key: 'token');
+      print('Token retrieved: $token'); // Log untuk debugging
+      if (token == null) {
+        Get.snackbar('Error', 'Token not found');
+        isLoading(false); // Set isLoading to false when there's an error
+        return;
+      }
+      final response = await http.get(
+        Uri.parse('$url/profile'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        print('Response data: $responseData'); // Log untuk debugging
+
+        profile.value = Profie.fromMap(responseData); // pastikan sesuai dengan data yang diterima
+      } else {
+        Get.snackbar('Error', 'Failed to load profile: ${response.body}');
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'An error occurred: $e');
+    } finally {
+      isLoading(false);
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  void logout() async {
+    await storage.delete(key: 'token');
+    Get.offAllNamed(Routes.LOGIN);
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,23 +1,191 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import '../controllers/profile_controller.dart';
 
 class ProfileView extends GetView<ProfileController> {
-  const ProfileView({Key? key}) : super(key: key);
+  const ProfileView({super.key});
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ProfileView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ProfileView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Obx(() {
+          if (controller.isLoading.value) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      Stack(
+                        children: [
+                          CircleAvatar(
+                            radius: 80,
+                            backgroundImage:
+                                NetworkImage(controller.profile.value.photo),
+                          ),
+                          Positioned(
+                            bottom: -10,
+                            left: 100,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: IconButton(
+                                onPressed: () {
+                                  showModalBottomSheet(
+                                    context: context,
+                                    builder: (_) {
+                                      return Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          ListTile(
+                                            leading: const Icon(Icons.camera),
+                                            title: const Text('Camera'),
+                                            onTap: () {
+                                              // Implementasi pengambilan gambar dari kamera
+                                              Get.back();
+                                            },
+                                          ),
+                                          ListTile(
+                                            leading:
+                                                const Icon(Icons.photo_album),
+                                            title: const Text('Gallery'),
+                                            onTap: () {
+                                              // Implementasi pengambilan gambar dari galeri
+                                              Get.back();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                icon: const Icon(Icons.add_a_photo_rounded),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        controller.profile.value.name,
+                        style: GoogleFonts.roboto(
+                          fontSize: size * 0.040,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        controller.profile.value.id.toString(),
+                        style: GoogleFonts.roboto(
+                          fontSize: size * 0.030,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ElevatedButton.icon(
+                            onPressed: () {},
+                            label: Text(
+                              "Edit Profile",
+                              style: GoogleFonts.roboto(
+                                fontSize: size * 0.030,
+                              ),
+                            ),
+                            icon: const Icon(
+                              Icons.edit,
+                              size: 15,
+                            ),
+                          ),
+                          ElevatedButton.icon(
+                            onPressed: () {},
+                            label: Text(
+                              "Hapus Akun",
+                              style: GoogleFonts.roboto(
+                                fontSize: size * 0.030,
+                              ),
+                            ),
+                            icon: const Icon(
+                              Icons.close,
+                              size: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Alamat',
+                  style: GoogleFonts.roboto(
+                    fontSize: size * 0.025,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(controller.profile.value.address),
+                const Divider(),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'No. HP',
+                  style: GoogleFonts.roboto(
+                    fontSize: size * 0.025,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(controller.profile.value.phone),
+                const Divider(),
+                const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                    onPressed: () {
+                      controller.logout();
+                    },
+                    label: Text(
+                      "Keluar",
+                      style: GoogleFonts.roboto(
+                        fontSize: size * 0.030,
+                        color: Colors.white,
+                      ),
+                    ),
+                    icon: const Icon(
+                      Icons.logout,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+        }),
       ),
     );
   }
