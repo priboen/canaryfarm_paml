b55097dffd3d2b0d2ddf1e043995651a0b4f0eb9
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-06-13 15:26:55 +0700

    make model canary type

---
+++ lib/app/data/canary_type.dart
@@ -0,0 +1,14 @@
+class CanaryType {
+  static final List<String> canaryItems = [
+    'Yorkshire',
+    'F1 Yorkshire',
+    'F2',
+    'F3',
+    'F4',
+    'F5',
+    'F6',
+    'AF',
+    'AFS',
+    'Lokal',
+  ];
+}
