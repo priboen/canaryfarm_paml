a4eb7e8c271128c51997d0aed3f6c6353d1c0286
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-06-15 18:11:58 +0700

    create dropdown for choosing Bird Parents

--- lib/app/modules/chicks/controllers/chicks_controller.dart
+++ lib/app/modules/chicks/controllers/chicks_controller.dart
@@ -1,23 +1,45 @@
 import 'package:get/get.dart';
+import 'package:royal_canary_farm_app/app/data/bird_parent.dart';
+import 'package:royal_canary_farm_app/app/modules/chicks/providers/parent_chicks_provider_provider.dart';
 
 class ChicksController extends GetxController {
-  //TODO: Implement ChicksController
+  var males = <BirdParent>[].obs;
+  var females = <BirdParent>[].obs;
+  var selectedMale = Rxn<BirdParent>();
+  var selectedFemale = Rxn<BirdParent>();
+  var isLoadingMales = true.obs;
+  var isLoadingFemales = true.obs;
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    fetchMales();
+    fetchFemales();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void fetchMales() async {
+    try {
+      isLoadingMales(true);
+      var maleList =
+          await ParentChicksProviderProvider.fetchParentsByGender('jantan');
+      if (maleList != null) {
+        males.value = maleList;
+      }
+    } finally {
+      isLoadingMales(false);
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  void fetchFemales() async {
+    try {
+      isLoadingFemales(true);
+      var femaleList =
+          await ParentChicksProviderProvider.fetchParentsByGender('betina');
+      if (femaleList != null) {
+        females.value = femaleList;
+      }
+    } finally {
+      isLoadingFemales(false);
+    }
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/chicks/views/chicks_view.dart
+++ lib/app/modules/chicks/views/chicks_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:royal_canary_farm_app/app/modules/widget/canary_type_dropdown.dart';
 
 import '../controllers/chicks_controller.dart';
 
@@ -10,14 +11,70 @@ class ChicksView extends GetView<ChicksController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChicksView'),
-        centerTitle: true,
+        title: Text('Daftar Orang Tua Burung'),
       ),
-      body: Center(
-        child: Text(
-          'ChicksView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Obx(
+        () {
+          if (controller.isLoadingMales.value ||
+              controller.isLoadingFemales.value) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  CanaryTypeDropdown(
+                    items: controller.males
+                        .map((male) => male.ringNumber)
+                        .toList(),
+                    hint: 'Pilih Ayah',
+                    onChanged: (selectedValue) {
+                      // Implement logic when male is selected
+                      controller.selectedMale.value = controller.males
+                          .firstWhere(
+                              (male) => male.ringNumber == selectedValue);
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  CanaryTypeDropdown(
+                    items: controller.females
+                        .map((female) => female.ringNumber)
+                        .toList(),
+                    hint: 'Pilih Ibu',
+                    onChanged: (selectedValue) {
+                      // Implement logic when female is selected
+                      controller.selectedFemale.value = controller.females
+                          .firstWhere(
+                              (female) => female.ringNumber == selectedValue);
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        if (controller.selectedMale.value != null)
+                          ListTile(
+                            title: Text(
+                                'Ayah Terpilih: ${controller.selectedMale.value!.ringNumber}'),
+                            subtitle: Text(
+                                'Tipe: ${controller.selectedMale.value!.canaryType}'),
+                          ),
+                        if (controller.selectedFemale.value != null)
+                          ListTile(
+                            title: Text(
+                                'Ibu Terpilih: ${controller.selectedFemale.value!.ringNumber}'),
+                            subtitle: Text(
+                                'Tipe: ${controller.selectedFemale.value!.canaryType}'),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
