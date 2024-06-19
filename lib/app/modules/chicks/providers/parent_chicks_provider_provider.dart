5ffa6833c3f411a6184731edd5461b5dfa408841
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-06-20 00:11:48 +0700

    returns the token value to the backend for data access

--- lib/app/modules/chicks/providers/parent_chicks_provider_provider.dart
+++ lib/app/modules/chicks/providers/parent_chicks_provider_provider.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:royal_canary_farm_app/api/service/constant.dart';
 import 'package:royal_canary_farm_app/app/data/bird_parent.dart';
@@ -7,20 +9,23 @@ import 'package:http/http.dart' as http;
 
 class ParentChicksProviderProvider extends GetConnect {
   static Future<List<BirdParent>?> fetchParentsByGender(String gender) async {
-    final response = await http.get(Uri.parse('$url/canary-list/$gender'));
+    final token = await FlutterSecureStorage().read(key: 'token');
+    final response = await http.get(
+      Uri.parse('$url/canary-list/$gender'),
+      headers: {
+        HttpHeaders.authorizationHeader: 'Bearer $token',
+      },
+    );
 
     if (response.statusCode == 200) {
-      // Periksa apakah respons berisi data dalam bentuk daftar
       var jsonResponse = json.decode(response.body);
       if (jsonResponse['data'] is List) {
         List data = jsonResponse['data'];
         return data.map((parent) => BirdParent.fromJson(parent)).toList();
       } else {
-        // Jika respons bukan daftar, lempar kesalahan atau tangani sesuai kebutuhan
         throw TypeError();
       }
     } else {
-      // Jika ada kesalahan, return null atau throw exception
       return null;
     }
   }
