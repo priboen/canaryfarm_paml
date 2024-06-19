81a60ca6daaa336bf689cb157933abc9c3185b62
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-06-20 00:13:25 +0700

    fix the typo

--- lib/app/modules/home/provider/profile_provider.dart
+++ lib/app/modules/home/provider/profile_provider.dart
@@ -3,11 +3,11 @@ import 'package:royal_canary_farm_app/api/service/constant.dart';
 import 'package:royal_canary_farm_app/app/data/profile.dart';
 
 class ProfileProvider {
-  Future<Profie> getProfile() async {
+  Future<Profile> getProfile() async {
     final response = await http.get(Uri.parse('$url/profile'));
 
     if (response.statusCode == 200) {
-      return Profie.fromJson(response.body);
+      return Profile.fromJson(response.body);
     } else {
       throw Exception('Failed to load profile');
     }
